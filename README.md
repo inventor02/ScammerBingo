31dcc7274089b367495388fcc3313b91f1639215
George Peppard <iaminventor02@gmail.com>
2018-02-06 21:02:56 +0000

    add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+![Logo](https://storage.googleapis.com/cdn.georgepeppard.me/ScammerBingo/text.png)
+---
+Scammer Bingo is a fun game for scambaiters. Every time the scammer says or does something on the board, you click the button. Get them to say or do everything and you win! That's it!
--- ScammerBingo/BingoForm.vb
+++ ScammerBingo/BingoForm.vb
@@ -1,5 +1,8 @@
-﻿Public Class BingoForm
+﻿Imports Squirrel
+
+Public Class BingoForm
     Dim score As Integer
+    Dim isWin32 As Boolean
 
     Private Sub HandleBingoButtonClick(sender As Button, e As EventArgs) Handles Button1.Click, Button9.Click, Button8.Click, Button7.Click, Button6.Click, Button5.Click, Button4.Click, Button3.Click, Button20.Click, Button2.Click, Button19.Click, Button18.Click, Button17.Click, Button16.Click, Button15.Click, Button14.Click, Button13.Click, Button12.Click, Button11.Click, Button10.Click
         Console.WriteLine("Button clicked: " & sender.Text)
@@ -10,8 +13,35 @@
         SetScore(score)
     End Sub
 
+    Async Sub UpdateApp()
+        If isWin32 Then
+            If Debugger.IsAttached Then
+                Console.WriteLine("Debugger attached - not updating")
+                Return
+            End If
+
+            Console.WriteLine("Checking for updates with Squirrel...")
+            Dim updateManager As UpdateManager = New UpdateManager("http://update.bingo.georgepeppard.me")
+            Await updateManager.UpdateApp()
+        Else
+            Console.WriteLine("Not updating with Squirrel - the Windows Store will handle updates!")
+        End If
+    End Sub
+
     Private Sub BingoForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         score = 0
+
+        Console.WriteLine("Getting application context")
+        Dim dbHelper As DesktopBridge.Helpers = New DesktopBridge.Helpers()
+        If dbHelper.IsRunningAsUwp() Then
+            isWin32 = False
+            Console.WriteLine("Running under UWP")
+        Else
+            isWin32 = True
+            Console.WriteLine("Running under Win32")
+        End If
+
+        UpdateApp()
     End Sub
 
     Sub SetScore(ByRef newScore As Integer)
